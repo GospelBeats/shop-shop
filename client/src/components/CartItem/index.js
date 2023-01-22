4bd3e111387383df3ef80d0f8d91342841b7587f
Cory Walker <cyb12345@comcast.net>
2023-01-22 18:01:35 -0500

    shopping cart done

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART } from '../../utils/actions';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 const CartItem = ({item}) => {
 
@@ -11,6 +11,23 @@ const CartItem = ({item}) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+  };
+
+  const onChange = (e) => {
+    const value = e.target.value;
+
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+    }
   };
 
     return (
@@ -29,6 +46,7 @@ const CartItem = ({item}) => {
                 type="number"
                 placeholder="1"
                 value={item.purchaseQuantity}
+                onChange={onChange}
               />
               <span
                 role="img"
