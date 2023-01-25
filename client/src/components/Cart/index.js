7cc8da7bb012a18d870c84b6af94f8d6d2040630
Cory Walker <cyb12345@comcast.net>
2023-01-25 09:21:29 -0500

    shopping cart offline data done

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,11 +3,24 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import {useStateContext, useStoreContext} from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { useEffect } from 'react';
+import { idbPromise } from '../../utils/helpers';
+
 
 const Cart = () => {
 
 const [state, dispatch] = useStoreContext();
+
+useEffect(() => {
+  async function getCart() {
+    const cart = await idbPromise('cart', 'get');
+    dispatch({type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+  };
+  if (!state.cart.length) {
+    getCart();
+  }
+}, [state.cart.length, dispatch]);
 
 function toggleCart() {
     dispatch({type: TOGGLE_CART});
